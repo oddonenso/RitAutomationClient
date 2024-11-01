8b887cf9a0f7cdb7eaef4e05664e6045b5d301f6
Daniil Molotkov <dmolotkov@rit-it.com>
2024-11-01 15:42:37 +0700

    fdsdfsdf

--- SuperServerRIT/Program.cs
+++ SuperServerRIT/Program.cs
@@ -1,105 +1,92 @@
-//using Microsoft.AspNetCore.Authentication.JwtBearer;
-//using Microsoft.IdentityModel.Tokens;
-//using System.Text;
-//using Microsoft.EntityFrameworkCore;
-//using Data;
-//using SuperServerRIT.Services;
-//using MediatR;
-//using SuperServerRIT.Commands; 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using SuperServerRIT.Services;
+using MediatR;
+using SuperServerRIT.Commands;
 
-//var builder = WebApplication.CreateBuilder(args);
-
-
-//builder.Services.AddDbContext<Connection>(options =>
-//    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+var builder = WebApplication.CreateBuilder(args);
 
 
-//builder.Services.AddMediatR(typeof(Program).Assembly);
+builder.Services.AddDbContext<Connection>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
-//builder.Services.AddScoped<IEquipmentRepository, EquipmentRepository>();
+builder.Services.AddMediatR(typeof(Program).Assembly);
 
-//builder.Services.AddControllers();
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
-//builder.Services.AddSingleton<RabbitMqService>();
-//builder.Services.AddHostedService<RabbitMqHostedService>();
-////dfshjgsdfhjsdfgjikhjksdfgikogjhjkhjkdfgjkhdfgjgdfjdfgjkfgjjgfjkdfg
-//dfgikhdfgujikdfghjikdfgj
+builder.Services.AddScoped<IEquipmentRepository, EquipmentRepository>();
 
-//    dfg
-//    dfg
-//    dfg
-//    dfg
-//    dfg
-//    dfg
-//    dfg
-//    dfg
-//    fg
-//    dfg
-//    g
-//var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-//builder.Services.AddAuthentication(options =>
-//{
-//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-//})
-//.AddJwtBearer(options =>
-//{
-//    options.RequireHttpsMetadata = false; 
-//    options.SaveToken = true;
-//    options.TokenValidationParameters = new TokenValidationParameters
-//    {
-//        ValidateIssuer = true,
-//        ValidateAudience = true,
-//        ValidateLifetime = true,
-//        ValidateIssuerSigningKey = true,
-//        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-//        ValidAudience = builder.Configuration["Jwt:Audience"],
-//        IssuerSigningKey = new SymmetricSecurityKey(key),
-//        ClockSkew = TimeSpan.Zero 
-//    };
-//});
+builder.Services.AddSingleton<RabbitMqService>();
+builder.Services.AddHostedService<RabbitMqHostedService>();
 
-//builder.Services.AddScoped<JwtService>();
+var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]);
 
-//// Настройка CORS для локального WPF-клиента
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy("AllowWpfClient", builder =>
-//        builder.WithOrigins("http://localhost") 
-//               .AllowAnyMethod()
-//               .AllowAnyHeader()
-//               .AllowCredentials()); 
-//});
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ClockSkew = TimeSpan.Zero
+    };
+});
 
-//var app = builder.Build();
+builder.Services.AddScoped<JwtService>();
 
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI(c =>
-//    {
-//        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SuperServerRitAPI V1");
-//        c.RoutePrefix = string.Empty; 
-//    });
-//}
+// Настройка CORS для локального WPF-клиента
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowWpfClient", builder =>
+        builder.WithOrigins("http://localhost")
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials());
+});
 
-//// Подключаем CORS
-//app.UseCors("AllowWpfClient");
+var app = builder.Build();
 
-//app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SuperServerRitAPI V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
 
-//app.UseAuthentication();
-//app.UseAuthorization();
+// Подключаем CORS
+app.UseCors("AllowWpfClient");
 
-//app.MapControllers();
+app.UseHttpsRedirection();
 
-//app.Run();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
